053136ddc463b9640fd28fa72664e59898fc9740
65836713 <hang.zhang@iss-ms.com>
2015-09-26 17:28:26 +0800

    数据结构

---
+++ 数据结构/Project1/Project1/Projects.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project1
+{
+    /// <summary>
+    /// 项目
+    /// </summary>
+    public interface Projects:Products,SalesManagers,ResponsiblePerson
+    {
+        string ProjectNumber { get; set; }//项目编号
+        string ProjectName { get; set; }//项目名称
+        string CustomerName { get; set; }//客户名称
+        string ProjectSddress { get; set; }//项目地址
+        string SalesManager { get; set; }//销售经理
+        string ContactPersonName { get; set; }//联系人姓名
+        int ContactPersonPhoneNumber { get; set; }//联系人电话
+        string Remarks { get; set; }//项目备注
+        DateTime DeliveryTime { get; set; }//要求交货时间
+        Products[] products { get; set; }//产品集合
+        SalesManagers[] salesmanagers { get; set; }//销售经理集合
+        ResponsiblePerson[] responsibleperson { get; set; }//项目负责人集合
+
+    }
+}
---
+++ 数据结构/Project1/Project1/ResponsiblePerson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project1
+{
+    /// <summary>
+    /// 项目负责人
+    /// </summary>
+    public interface ResponsiblePerson
+    {
+        string ResponsiblePersonName { get; set; }//负责人名称
+        int ResponsiblePersonPhoneNumber { get; set; }//负责人电话
+        string ResponsiblePersonEmail { get; set; }//负责人邮箱
+    }
+}
