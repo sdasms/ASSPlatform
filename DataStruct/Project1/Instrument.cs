863fccfdb79a98d76e114fa052c906c88b245219
65836713 <hang.zhang@iss-ms.com>
2015-10-13 15:47:38 +0800

    20151013

---
+++ DataStruct/Project1/Instrument.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project1
+{
+    /// <summary>
+    /// 系统
+    /// </summary>
+    public class Instrument
+    {
+        string UID { get; set; }//唯一识别编号
+        string ProjectNumber { get; set; }//所属项目编号-用于索引所属项目并获取相关信息
+        string ProjectName { get; set; }//所属项目名称
+        string Location { get; set; }//安装位置
+        string Type { get; set; }//仪器类型  DTS  气体
+        State State { get; set; }//仪器状态-维修状态
+        string Model { get; set; }//产品型号-备用
+        string Remarks { get; set; }//备注
+        bool Online { get; set; }//在线状态
+        string DeliveryStatus;//发货状态
+        DateTime FactoryTime;//出厂时间
+        DateTime FaultTime;//故障发生时间
+        int CuePeriod;//下次提示推送周期
+        DateTime DelayTime;//延期到期时间
+        public List<DutyContactPair> Dutycontactpair;//联系人
+    }
+
+    public enum State
+    {
+        //故障
+        //正在维修
+        //正常
+    }
+
+    /// <summary>
+    /// 联系人
+    /// </summary>
+    public class DutyContactPair
+    {
+        public Contact Contact { get; set; }
+        public Duty Duty { get; set; }
+    }
+
+    /// <summary>
+    /// 联系人类型
+    /// </summary>
+    public enum Duty
+    {
+        //硬件负责人
+        //软件
+        //系统负责人
+        //销售和工程在项目里-系统发生问题时如何推送给销售和工程
+    }
+}
--- DataStruct/Project1/Project.cs
+++ DataStruct/Project1/Project.cs
@@ -8,20 +8,23 @@ namespace Project1
     /// <summary>
     /// 项目
     /// </summary>
-    public interface Projects:Products,SalesManagers,ResponsiblePerson
+    public class Project
     {
-        string ProjectNumber { get; set; }//项目编号
+        string ProjectNumber { get; set; }//项目编号-唯一
         string ProjectName { get; set; }//项目名称
         string CustomerName { get; set; }//客户名称
         string ProjectSddress { get; set; }//项目地址
-        string SalesManager { get; set; }//销售经理
+        Contact SalesManager { get; set; }//销售经理
+        Contact ProjectManager { get; set; }//工程负责人
         string ContactPersonName { get; set; }//联系人姓名
         int ContactPersonPhoneNumber { get; set; }//联系人电话
         string Remarks { get; set; }//项目备注
+        string ProjectState { get; set; }//项目状态：正常、维修
         DateTime DeliveryTime { get; set; }//要求交货时间
-        Products[] products { get; set; }//产品集合
-        SalesManagers[] salesmanagers { get; set; }//销售经理集合
-        ResponsiblePerson[] responsibleperson { get; set; }//项目负责人集合
+        List<Instrument> Instrument { get; set; }//系统集合
+
+        List<Record> Record;//维修记录;
+
 
     }
 }
